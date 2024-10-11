85f6c4539f36cef18db0d73a3cb7839112032027
skyman2413 <stepan26062001@gmail.com>
2024-10-11 18:52:55 +0300

    echo server

--- lvl1/simple_http_server/main.go
+++ lvl1/simple_http_server/main.go
@@ -8,17 +8,19 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
-		defer r.Body.Close()
-		rBody, err := io.ReadAll(r.Body)
-		if err != nil {
-			log.Printf("Error reading body: %s", err)
-			return
-		}
-		var rStr = string(rBody)
-		log.Print("Getted request ", r.Method, " on ", r.URL.Path, " with content ", rStr)
-		io.Copy(w, bytes.NewBuffer(rBody))
-	})
+	http.HandleFunc("/echo", EchoRequest)
 	log.Print("Listening on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func EchoRequest(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	rBody, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Printf("Error reading body: %s", err)
+		return
+	}
+	var rStr = string(rBody)
+	log.Print("Getted request ", r.Method, " on ", r.URL.Path, " with content ", rStr)
+	io.Copy(w, bytes.NewBuffer(rBody))
 }
